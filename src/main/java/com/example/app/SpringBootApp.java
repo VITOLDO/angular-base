6c77d212e01cd0795ed23b0d88f02158c4d73407
vfilonenko <viktorfilonenko@gmail.com>
2017-07-07 14:37:06 +0300

    adding spring boot and gradle

---
+++ src/main/java/com/example/app/SpringBootApp.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vfilonenko on 07.07.2017.
+ */
+@SpringBootApplication
+public class SpringBootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+
+}
