9c2d96068bc8f803cde27072e4e61d102b1ec109
vfilonenko <viktorfilonenko@gmail.com>
2017-07-07 14:53:30 +0300

    time input with mask

--- src/main/resources/static/admintool/sysprop/sysprop.controller.js
+++ src/main/resources/static/admintool/sysprop/sysprop.controller.js
@@ -17,7 +17,12 @@ function SyspropController($resource, $scope, $http, $base64, ngToast) {
         vm.syspropertiesMap = {};
         vm.syspropertiesData = $resource('http://192.168.99.100:9090/papi/services/system/properties/:id', {id:vm.thisApp}).query().$promise.then(function(array) {
             array.forEach(function(item) {
-                vm.syspropertiesMap[item.key] = item.value;
+                if (item.key == 'ADM.MN_HISTORY_TIMEOUT' || item.key == 'ADM.MN_USER_ACTIVE_TIMEOUT') {
+                    console.log(item.value)
+                    vm.syspropertiesMap[item.key] = new Date(1970, 0, 1).setSeconds(item.value/1000);
+                } else {
+                    vm.syspropertiesMap[item.key] = item.value;
+                }
             })
         });
     }
